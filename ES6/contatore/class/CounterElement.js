a4bcf3dcb0e567b777b0f004083beb8af023c269
mnb89 <mnb.alter89@gmail.com>
2021-04-11 15:46:40 +0200

    new CounterSpec all true

---
+++ ES6/contatore/class/CounterElement.js
@@ -0,0 +1,8 @@
+import CounterView from "./CounterView.js";
+
+
+const counterA = new CounterView('#counterA')
+const counterB = new CounterView('#counterB')
+const counterC = new CounterView('#counterC')
+const counterD = new CounterView('#counterD')
+const counterE = new CounterView('#counterE')
